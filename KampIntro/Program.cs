b9610f5fbbc51d5859aed49d256982cdb2f22ab6
Eren Özcan <75170547+Eren-Ozcan@users.noreply.github.com>
2021-03-05 10:24:27 +0300

    Add project files.

---
+++ KampIntro/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace KampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string kategoriEtiketi = "Kategoriler";
+            int ogrenciSayisi = 32000;
+            double faizOrani = 1.45;
+            bool sistemeGirisYapmismi = true;
+            double dolarBugun = 7.70;
+            double dolarDun = 7.60;
+            if (sistemeGirisYapmismi == true)
+            {
+                Console.WriteLine("Kullanıcı ayarları butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş yap butonu");
+            }
+            if (dolarBugun>dolarDun)
+            {
+                Console.WriteLine("Artıs oku");
+            }
+            else if (true)
+            {
+                Console.WriteLine("Azalıs oku");
+            }
+            else
+            {
+                Console.WriteLine("Degismedi");
+            }
+
+            if (sistemeGirisYapmismi == true)
+            {
+                Console.WriteLine("Kullanıcı ayarları butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giris yap butonu");
+            }
+            Console.WriteLine(kategoriEtiketi);
+        }
+    }
+}
---
+++ metotlar/SepetManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace metotlar
+{
+    class SepetManager
+    {
+        public void Ekle(Urun urun) 
+        {
+            Console.WriteLine("Tebrikler. Sepete eklendi : " + urun.Adi);
+
+
+        }
+
+        public void Ekle2(string urunAdi, string aciklma, double fiyat, int stokAdedi)
+        {
+            Console.WriteLine("Tebrikler. Sepete eklendi : " + urunAdi);
+        }
+    }
+}
